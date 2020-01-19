da6ca650d1cd9041d1ae37125d186335940de2d5
Tyler Porter <tyler.b.porter@gmail.com>
2020-01-19 17:10:14 -0500

    Output race details to Reddit

--- bots/bot.py
+++ bots/bot.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python
 
-import os, pdb, praw, sys
+import os, pdb, praw, re, sys
 sys.path.append('../')
 from pymongo import MongoClient
 from env import EnvVarSetter
+from datetime import datetime as dt
 
 class GravelCyclingBot():
   def __init__(self):
     EnvVarSetter().set_vars()
     self.reddit_instance = self.setup()
+    self.subreddit = self.reddit_instance.subreddit(os.environ['REDDIT_SUBREDDIT'])
 
   def setup(self):
     reddit = praw.Reddit(client_id=os.environ['REDDIT_CLIENT_ID'],
@@ -21,8 +23,30 @@ class GravelCyclingBot():
 
   def fetch_events(self):
     db_client = MongoClient(os.environ['MONGO_CONNECT_URL'])
+    events = db_client.gravel_cycling.events
 
-  def create_post(self):
-    pass
+    return events
 
-print( GravelCyclingBot().reddit_instance )
+  def build_text(self, events):
+    event_text = ''
+    for event in events.find().limit(10):
+      try:
+        event_text += event['description']
+        event_text += '\n'
+      except Exception:
+        pass
+
+    newline = re.compile('\\n')
+    return re.sub(newline, '\n\n', event_text)
+
+  def create_monthly_post(self):
+    title = 'Gravel Events for Month of {}'.format(dt.now().strftime('%B, %Y'))
+    events = self.fetch_events()
+    text = self.build_text(events)
+
+    self.subreddit.submit(title=title,
+                          selftext=text,
+                          send_replies=False)
+
+gcb = GravelCyclingBot()
+gcb.create_monthly_post()
